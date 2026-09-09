abcc246646276e3b83e18996affcbef5b07c0c68
Dmitry Girdyuk <dg_huawei@mail.ru>
2026-09-09 08:33:01 +0000

    no_lora acl graph fix

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -3283,10 +3283,7 @@ class NPUModelRunner(GPUModelRunner):
             num_scheduled_tokens,
             num_sampled_tokens,
             remove_lora,
-            # TODO: The next line is a temporary workaround
-            # to fix the accuracy issue of test_llama32_lora.py,
-            # which is introduced by vllm-project/vllm#32005
-            num_active_loras=(self.lora_config.max_loras if self.lora_config is not None else num_active_loras),
+            num_active_loras=num_active_loras,
         ):
             # Make sure padding doesn't exceed max_num_tokens
             assert num_tokens_padded <= self.max_num_tokens
